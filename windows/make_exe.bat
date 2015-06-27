d0a0dacf198332f11913e00f92f312ec3949602d
Jason Proctor <jason@redfish.net>
2015-06-27 16:18:34 -0700

    adding jexepack builder safety copy of jsmooth'd exe

---
+++ windows/make_exe.bat
@@ -0,0 +1,9 @@
+
+jexepack /appname:VectorSurgeon /out:VectorSurgeon.exe ^
+/minver:1.5 ^
+/maxver:1.8 ^
+/main:com.prophetvs.editor.ControlWindow ^
+/icon:win32\application.ico ^
+/windowed ^
+vs.jar
+
