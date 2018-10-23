42abdfb3a4f0b38b8b3716a4e173e9acd6ef3754
Jason Proctor <jason@redfish.net>
2018-10-23 15:31:59 -0700

    remove windows dirs and native wrappers from dist yay

--- windows/make_exe.bat
+++
@@ -1,9 +0,0 @@
-
-jexepack /appname:VectorSurgeon /out:VectorSurgeon.exe ^
-/minver:1.5 ^
-/maxver:1.8 ^
-/main:com.prophetvs.editor.ControlWindow ^
-/icon:VectorSurgeon.ico ^
-/windowed ^
-vs.jar
-
